7b1a58df8908a8af80304023ccaf87b58bfada5b
Serov Sergey <serovsergey79@gmail.com>
2022-08-02 15:06:43 +0300

    fix task 8 formData

--- js/task-08.js
+++ js/task-08.js
@@ -6,6 +6,7 @@ const onSubmit = (event) => {
     if (email.value === "" || password.value === "") {
         return alert("Please fill in all the fields!");
     }
+    // console.log(event.currentTarget.elements)
     // let obj = {};
     // for (const element of event.currentTarget.elements) {
     //     console.log(element)
@@ -15,9 +16,10 @@ const onSubmit = (event) => {
     // console.log(obj);
     const formData = new FormData(event.currentTarget);
     let obj = {};
-    for (const key of formData.keys()) {
-        obj[key] = formData.get(key);
-    }
+    // for (const [key, value] of formData) {
+    //     obj[key] = value;
+    // }
+    formData.forEach((value, key) => obj[key] = value);
     console.log(obj)
     event.currentTarget.reset();
 }
